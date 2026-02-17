a308c869c03d8a6d7b78366fb4b4ba3f022bab03
Sergii Sharmazanov <sergii.sharmazanov@wpp.com>
2026-02-17 08:56:18 +0100

    Multi messages

--- client.py
+++ client.py
@@ -5,15 +5,19 @@ HOST = os.getenv("ECHO_HOST", "127.0.0.1")
 PORT = int(os.getenv("ECHO_PORT", 9000))
 
 def main():
-    message = "Hello from client!\n"
-
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
-        s.sendall(message.encode("utf-8"))
-        data = s.recv(1024)
+        print("Connected. Type messages. Type 'quit' type exit.")
 
-    print("Sent:     ", message.strip())
-    print("Received: ", data.decode("utf-8").strip())
+        while True:
+            message = input("> ")
+            if message.lower() == "quit":
+                break
+
+            s.sendall((message + "\n").encode("utf-8"))
+            data = s.recv(1024)
+
+            print("Received: ", data.decode("utf-8").strip())
 
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -3,6 +3,17 @@ import socket
 HOST = "0.0.0.0"
 PORT = 9000
 
+def handle_client(conn, addr):
+    print(f"Connected by { addr }")
+    with conn:
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                print(f"Client disconnected: { addr }")
+                return
+            conn.sendall(data)
+
+
 def main():
     # AF_INET = IPv4, SOCK_STREAM = TCP
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -10,20 +21,12 @@ def main():
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
         s.bind((HOST, PORT))
-        s.listen(1)  # wait for up to 1 queued connection
-
+        s.listen()
         print(f"Echo server listening on {HOST}:{PORT} ...")
 
-        conn, addr = s.accept()  # blocks until a client connects
-        with conn:
-            print("Connected by", addr)
-            while True:
-                data = conn.recv(1024)  # read up to 1024 bytes
-                if not data:
-                    print("Client disconnected.")
-                    break
-                print("Received:", data)
-                conn.sendall(data)  # echo back exactly what we got
+        while True:
+            conn, addr = s.accept()  # blocks until a client connects
+            handle_client(conn, addr)
 
 if __name__ == "__main__":
     main()
