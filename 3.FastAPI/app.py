4b1e982b2949cf8f99bc81a11502b9db50249f52
Sergii Sharmazanov <sergii.sharmazanov@wpp.com>
2026-02-19 21:33:23 +0100

    Add pydantic model, status_code, query and path arguments

--- 3.FastAPI/app.py
+++ 3.FastAPI/app.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 app = FastAPI(title="My first API")
+
+
+class Task(BaseModel):
+    task: str
+    description: str | None = None
+    done: bool | None = False
+
 
 @app.get("/")
 def root():
@@ -10,4 +19,19 @@ def root():
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+
+@app.get("/add")
+def add(a: int, b: int):
+    return {"result": a + b}
+
+
+@app.get("/hello/{name}")
+def max(name: str):
+    return {"hello": name}
+
+
+@app.post("/tasks", status_code=201)
+def create_task(task: Task):
+    return task
 
